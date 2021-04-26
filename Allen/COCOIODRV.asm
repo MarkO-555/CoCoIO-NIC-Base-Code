a6e56178364cce5773cd8f98e95b6d582ad6972e
ExileInParadise <ExileInParadise@users.noreply.github.com>
2021-04-26 10:06:21 -0500

    Updated W5100_RESET to use MMIO port $FF68 and watch for bit 7 clear before config Updated COCOIODRV.asm to configure each data item as a seperate subroutine Updated COCOIODRV.asm to include each config subroutine in top-level jump table

--- Allen/COCOIODRV.asm
+++ Allen/COCOIODRV.asm
@@ -27,31 +27,32 @@
             org   $7E00
 RESET:      jmp   W5100_RST
 CONFIG:     jmp   W5100_CFG
+GATEWAY:    jmp   W5100_GATEWAY
+SUBNET:     jmp   W5100_SUBNET
+HARDWARE:   jmp   W5100_HARDWARE
+IPADDR:     jmp   W5100_IPADDR
+MPISLOT:    jmp   MPISLOT1
 
 W5100_RST:                    ; Reset the CoCoIO WIZnet 5100S
-            jsr   MPISLOT
-            ldd   #MR         ; W5100S Mode Register 0x0000
-            sta   CIO0ADDR    ; CoCoIO Address Register MSB
-            stb   CIO0ADDR+1  ; CoCoIO Address Register LSB
-            lda   CIO0DATA    ; Read the current value of MR
-            ora   #%10000011  ; Flip bit 7 RST to 1 = init all W5100S registers - autoclear in 3 SYS_CLK
-            pshs  A           ; Stash A for a few cycles
-            ldd   #MR         ; Load D with W5100S MR Register
-            sta   CIO0ADDR    ; CoCoIO Address Register MSB
-            stb   CIO0ADDR+1  ; CoCoIO Address Register LSB
-            puls  A           ; Now get A back
-            sta   CIO0DATA    ; Stuff the modified register back to trigger reset
+            jsr   MPISLOT1
+            lda   CIO0CMND    ; Read the current value of MR from CoCoIO Command
+            ora   #%10000000  ; Flip bit 7 RST to 1 = init all W5100S registers - autoclear in 3 SYS_CLK
+            sta   CIO0CMND    ; Trigger the reset
+RSTDONE:    lda   CIO0CMND    ; Now read command register to check bit 7 clears when reset is done
+            bmi   RSTDONE     ; if bit 7, then A was negative, keep checking bit
+            ora   #%00000011  ; bit 7 cleared, setup Ping Block disabled, no PPPoE, AutoIncrement, and Indirect Bus Mode
+            sta   CIO0CMND    ; configure the chip and done
             rts
 
 W5100_CFG:                    ; Configure the CoCoIO WIZnet W5100S
-            jsr   MPISLOT
-            ldd   #MR         ; W5100S Mode Register 0x0000
-            sta   CIO0ADDR    ; CoCoIO Address Register MSB
-            stb   CIO0ADDR+1  ; CoCoIO Address Register LSB
-            lda   #%00000011  ; No Reset, Ping Block disabled, No PPPoE, AutoIncrement, Indirect Bus I/F
-            sta   CIO0DATA    ; Setup the mode listed above
+            jsr   MPISLOT1
+            jsr   W5100_GATEWAY
+            jsr   W5100_SUBNET
+            jsr   W5100_HARDWARE
+            jsr   W5100_IPADDR
+            rts
 
-                              ; Mext configure the Gateway address
+W5100_GATEWAY:                ; Configure the Gateway address
             ldd   #GAR0       ; W5100S Gateway Address Register 0
             sta   CIO0ADDR    ; CoCoIO Address Register MSB
             stb   CIO0ADDR+1  ; CoCoIO Address Register LSB
@@ -62,8 +63,9 @@ GWRLOOP:    lda   ,X+         ; Load A with the next byte of Gateway
             incb              ; Increment loop counter
             cmpb  #4          ; Are we past the end of the Gateway data?
             bne   GWRLOOP     ; No, go back and do more
+            rts
 
-                              ; Next the Subnet Mask
+W5100_SUBNET:                 ; Next the Subnet Mask
             ldd   #SUBR0      ; W5100S Subnet Mask Address Register 0
             sta   CIO0ADDR    ; CoCoIO Address Register MSB
             stb   CIO0ADDR+1  ; CoCoIO Address Register LSB
@@ -74,8 +76,9 @@ SUBRLOOP:   lda   ,X+         ; Load A with the next byte of Subnet
             incb              ; Increment loop counter
             cmpb  #4          ; Are we past the end of the Subnet data?
             bne   SUBRLOOP    ; No, go back and do more
+            rts
 
-                              ; Next the Source Hardware Address
+W5100_HARDWARE:               ; Next the Source Hardware Address
             ldd   #SHAR0      ; W5100S Source Hardware Address Register 0
             sta   CIO0ADDR    ; CoCoIO Address Register MSB
             stb   CIO0ADDR+1  ; CoCoIO Address Register LSB
@@ -86,8 +89,9 @@ SHARLOOP:   lda   ,X+         ; Load A with the next byte of MAC data
             incb              ; Increment loop counter
             cmpb  #6          ; Are we past the end of the MAC data?
             bne   SHARLOOP    ; No, go back and do more
+            rts
 
-                              ; Next the Source IP Address
+W5100_IPADDR:                 ; Next the Source IP Address
             ldd   #SIPR0      ; W5100S Source IP Register 0
             sta   CIO0ADDR    ; CoCoIO Address Register MSB
             stb   CIO0ADDR+1  ; CoCoIO Address Register LSB
@@ -100,7 +104,7 @@ SIPRLOOP:   lda   ,X+         ; Load A with the next byte of IP data
             bne   SIPRLOOP    ; No, go back and do more
             rts
 
-MPISLOT:    lda   $FF7F       ; Read the MPI slot control
+MPISLOT1:   lda   $FF7F       ; Read the MPI slot control
             anda  #%11110000  ; zero out the SCS nybble for MPI slot 1
             sta   $FF7F       ; Write the updated SCS
             rts
